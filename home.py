4b80e0c45b2674e3e5c244c7b4b0a034b0c6d0b8
Sang Minh Pham <119334855+Sangqpham0102@users.noreply.github.com>
2024-11-27 22:01:21 +0700

    Update home.py

--- home.py
+++ home.py
@@ -89,4 +89,6 @@ def predict_emotion():
     return jsonify(prediction_data), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Lấy cổng từ biến môi trường
+    app.run(debug=True, host='0.0.0.0', port=port)
+
