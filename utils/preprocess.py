558eccfb53716f43b2aa20504a521f6e0a5f8640
Sang Minh Pham <119334855+Sangqpham0102@users.noreply.github.com>
2024-11-28 02:02:47 +0700

    Update preprocess.py

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,21 +1,18 @@
 import cv2
 import numpy as np
-import os
+import io
 
-def preprocess_image(image_path, target_size=(48, 48)):
+def preprocess_image(image_file, target_size=(48, 48)):
     """Tiền xử lý ảnh xám (grayscale), bao gồm phát hiện khuôn mặt và chuẩn hóa ảnh"""
-    
-    # Đảm bảo đường dẫn tệp Cascade Classifier là hợp lệ
-    face_cascade_path = 'models/haarcascade_frontalface_default.xml'
-    if not os.path.exists(face_cascade_path):
-        raise FileNotFoundError(f"Face cascade file not found at {face_cascade_path}")
-    
-    face_cascade = cv2.CascadeClassifier(face_cascade_path)
-    
-    # Đọc ảnh từ đường dẫn
-    image = cv2.imread(image_path)
+    # Đọc dữ liệu ảnh từ BytesIO
+    image_bytes = image_file.read()
+    nparr = np.frombuffer(image_bytes, np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
     if image is None:
-        raise FileNotFoundError(f"Image not found at {image_path}")
+        raise ValueError("Image could not be read")
+
+    face_cascade = cv2.CascadeClassifier('models/haarcascade_frontalface_default.xml')
     
     # Chuyển ảnh sang grayscale
     image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
